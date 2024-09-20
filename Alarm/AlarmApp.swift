146d4eec86f098a299c90b1b7213d7ca2cdddaa5
uwi0 <kakapoweb@gmail.com>
2024-09-20 15:42:11 +0700

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by dwi prasetyo on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlarmTests/AlarmTests.swift
@@ -0,0 +1,16 @@
+//
+//  AlarmTests.swift
+//  AlarmTests
+//
+//  Created by dwi prasetyo on 20/09/24.
+//
+
+import Testing
+
+struct AlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
