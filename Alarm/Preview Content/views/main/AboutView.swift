2f3f8e356b139c79985fa3533ff71e2c377aa716
uwi0 <kakapoweb@gmail.com>
2024-09-22 14:09:19 +0700

    completed about view

---
+++ Alarm/Preview Content/views/main/AboutView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        ZStack {
+            MainGradientView(endRadius: screenHeight / 2)
+                .ignoresSafeArea()
+            VStack {
+                VStack(spacing: 30) {
+                    CoolTextView(text: "About", size: 48)
+                    Text("The UI for this nice Alarm app was largely inspired by the amazing work of Anton Leogky.")
+                        .font(.title)
+                }
+                .multilineTextAlignment(.leading)
+                .minimumScaleFactor(0.1)
+                .padding()
+                Spacer()
+                Image(appImageSamples)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(5)
+                    .frame(width: screenWidth * 0.8)
+                Spacer()
+                
+                if let url = URL(string: "https://dribbble.com/antonleogky") {
+                    Link(destination: url, label: {
+                        ButtonView(text: "to Anton's page")
+                            .padding()
+                    })
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AboutView()
+}
