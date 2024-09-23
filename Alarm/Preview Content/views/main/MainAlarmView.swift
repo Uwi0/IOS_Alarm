de9871b68e6228f51b3d4528b40203dcffcbbec0
uwi0 <kakapoweb@gmail.com>
2024-09-23 13:37:28 +0700

    creating tab view in main alarm view

--- Alarm/Preview Content/views/main/MainAlarmView.swift
+++ Alarm/Preview Content/views/main/MainAlarmView.swift
@@ -2,13 +2,16 @@ import SwiftUI
 
 struct MainAlarmView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            AddEditAlarmView(currentAlarmIndex: nil, alarmModel: .DefaultAlarm())
+                .tabItem {
+                    Label("Alarm", systemImage: "alarm.fill")
+                }
+            AboutView()
+                .tabItem {
+                    Label("About", systemImage: "info.circle.fill")
+                }
         }
-        .padding()
     }
 }
 
