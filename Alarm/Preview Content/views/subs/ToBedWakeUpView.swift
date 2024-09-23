bb4a950625c004733356f4e2ca1f3d2d4b5ed51b
uwi0 <kakapoweb@gmail.com>
2024-09-23 19:03:40 +0700

    change implementation of Button select sound into context menu

--- Alarm/Preview Content/views/subs/ToBedWakeUpView.swift
+++ Alarm/Preview Content/views/subs/ToBedWakeUpView.swift
@@ -52,30 +52,34 @@ struct ToBedWakeUpView: View {
                     }
                     GridRow {
                         Text("")
-                        HStack {
-                            Text("Sound")
-                                .fontWeight(.semibold)
-                            Text(alarmModel.sound.rawValue)
-                                .font(.caption)
-                                .fontWeight(.thin)
-                        }
-                        .padding(7)
-                        .overlay {
-                            Capsule().stroke()
-                        }
-                        .contextMenu {
-                            ForEach(Sounds.allCases, id: \.self) { sound in
-                                Button(
-                                    action: {
-                                        alarmModel.sound = sound
-                                    },
-                                    label: {
-                                        Text(sound.rawValue)
-                                    }
-                                )
+                        Menu(
+                            content: {
+                                ForEach(Sounds.allCases, id: \.self) { sound in
+                                    Button(
+                                        action: {
+                                            alarmModel.sound = sound
+                                        },
+                                        label: {
+                                            Text(sound.rawValue)
+                                        }
+                                    )
+                                }
+                                .padding(.vertical)
+                            },
+                            label: {
+                                HStack {
+                                    Text("Sound")
+                                        .fontWeight(.semibold)
+                                    Text(alarmModel.sound.rawValue)
+                                        .font(.caption)
+                                        .fontWeight(.thin)
+                                }
+                                .padding(7)
+                                .overlay {
+                                    Capsule().stroke()
+                                }
                             }
-                            .padding(.vertical)
-                        }
+                        )
                     }
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
