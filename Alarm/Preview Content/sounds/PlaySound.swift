a125e60c7d39a0dfa3be198d78ace6c97144338b
uwi0 <kakapoweb@gmail.com>
2024-09-24 13:10:27 +0700

    create PlaySound

---
+++ Alarm/Preview Content/sounds/PlaySound.swift
@@ -0,0 +1,20 @@
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer? = AVAudioPlayer()
+
+func playSOund(sound: String, type: String = "", volume: Float = 1.0) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.setVolume(volume, fadeDuration: 0.1)
+            audioPlayer?.play()
+        }catch let error {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+}
+
+func stopSound() {
+    audioPlayer?.stop()
+}
